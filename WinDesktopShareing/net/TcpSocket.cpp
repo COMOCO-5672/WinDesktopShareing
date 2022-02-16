b1dac832aac4f28f247912c570c5a366261e7838
jiaopeilun@iseetech.com.cn <jiaopeilun@iseetech.com.cn>
2022-02-16 18:40:25 +0800

    update tcpsocket

---
+++ WinDesktopShareing/net/TcpSocket.cpp
@@ -0,0 +1,35 @@
+
+#include "TcpSocket.h"
+#include "Socket.h"
+
+using namespace xop;
+
+TcpSocket::TcpSocket(SOCKET sockfd) :sockfd_(sockfd)
+{
+
+}
+
+TcpSocket::~TcpSocket()
+{
+
+}
+
+SOCKET TcpSocket::Create()
+{
+    sockfd_ = ::socket(AF_INET, SOCK_STREAM, 0);
+    return sockfd_;
+}
+
+bool TcpSocket::Bind(std::string ip, uint16_t port)
+{
+    struct sockaddr_in addr = { 0 };
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(ip.c_str());
+    addr.sin_port = htons(port);
+    if (::bind(sockfd_, (struct sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR) {
+
+    }
+}
+
+
+
--- WinDesktopShareing/net/TcpSocket.h
+++ WinDesktopShareing/net/TcpSocket.h
@@ -12,10 +12,15 @@ namespace xop {
         virtual ~TcpSocket();
 
         SOCKET  Create();
-        bool    Bind();
-        bool    Listen();
+        bool    Bind(std::string ip, uint16_t port);
+        bool    Listen(int backlog);
         SOCKET  Accept();
-        bool    Connect();
+        bool    Connect(std::string ip, uint16_t port, int timeout = 0);
+        void    Close();
+        void    ShutdownWrite();
+        SOCKET  GetSocket() const { return sockfd_; }
+    private:
+        SOCKET  sockfd_ = -1;
     };
 }
 
